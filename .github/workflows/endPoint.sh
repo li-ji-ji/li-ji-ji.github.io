24457a5a458c109d51819da4d41b9a4c4c3aca10
lhj <1187382451@qq.com>
2021-01-22 11:11:10 +0800

    修改自动生成sitmap并提交 ---lhj

--- .github/workflows/endPoint.sh
+++ .github/workflows/endPoint.sh
@@ -3,7 +3,7 @@
 function saveSitemap()
 {
     find $1 -name '*.html' -type f -print0 | while IFS= read -r -d $'\0' file; 
-        do echo ${file/public/https://blog.evil-scream.cn} >> sitemap.txt
+        do echo ${file/public/https://blog.evil-scream.cn} >> updatesitemap.txt
     done
 }
 
